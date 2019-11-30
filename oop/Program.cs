41c77c74de8398a3ce8ea961a0c59ca081d230ac
Maks <belukiubog@gmail.com>
2019-11-30 22:55:01 +0200

    update

---
+++ oop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using operators_Library;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(1,2);
+            Fraction b = new Fraction(3,7);
+            Fraction suma = a + b;
+            Fraction difference = a - b;
+            Fraction product = a * b;
+            Fraction fraction = a / b;
+            Console.WriteLine($"a + b = {suma.ToString()}");
+            Console.WriteLine($"a - b = {difference.ToString()}");
+            Console.WriteLine($"a * b = {product.ToString()}");
+            Console.WriteLine($"a / b = {fraction.ToString()}");
+            if (a==b)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ operators/Fraction.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace operators_Library
+{
+    public class Fraction
+    {
+        Int64 Denominator, Numerator;
+        public Fraction(long num, long den)
+        {
+            if (den <= 0)
+            {
+                Numerator = -num;
+                Denominator = -den;
+            }
+            else
+            {
+                Numerator = num;
+                Denominator = den;
+            }
+        }
+        public Fraction(long num)
+        {
+            
+            Numerator = num;
+            Denominator = 1;
+        }
+        public Fraction(double num)
+        {
+            double k = 0;
+            double num1 = num;
+            while(num1%1>0)
+            {
+                num1 *= 10;
+                k++;
+            }
+            Numerator = (long)num1;
+            Denominator = (long)Math.Pow((double)10,k);
+            //Numerator = num;
+            //Denominator = 1;
+        }
+        public static Fraction operator +(Fraction a)
+        {
+            return new Fraction(a.Numerator, a.Denominator);
+        }
+        public static Fraction operator -(Fraction a)
+        {
+            return new Fraction(-a.Numerator, a.Denominator);
+        }
+       
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            long product = a.Denominator * b.Denominator;
+            long nsk = product /NSK(a.Denominator, b.Denominator);
+            Fraction res= new Fraction(a.Numerator*(nsk/a.Denominator)+b.Numerator*(nsk/b.Denominator),nsk);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            long product = a.Denominator * b.Denominator;
+            long nsk = product / NSK(a.Denominator, b.Denominator);
+            Fraction res = new Fraction(a.Numerator * (nsk / a.Denominator) - b.Numerator * (nsk / b.Denominator), nsk);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            Fraction res = new Fraction(a.Numerator *b.Numerator,a.Denominator*b.Denominator);
+            res.Simplify();
+            return res;
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            Fraction res= new Fraction(a.Numerator * b.Denominator, a.Denominator * a.Numerator);
+            res.Simplify();
+            return res;
+        }
+        public static bool operator >(Fraction a , Fraction b)
+        {
+            return (double)a > (double)b;
+        }
+        public static bool operator <(Fraction a, Fraction b)
+        {
+            return (double)a < (double)b;
+        }
+        public static bool operator >=(Fraction a, Fraction b)
+        {
+            return (double)a >= (double)b;
+        }
+        public static bool operator <=(Fraction a, Fraction b)
+        {
+            return (double)a <= (double)b;
+        }
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            return (double)a == (double)b;
+        }
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return (double)a > (double)b;
+        }
+        public void Simplify()
+        {
+           long num = Numerator,den=Denominator,nsk=0;
+            while(nsk!=1 && num!=0)
+            {
+                nsk = NSK(num, den);
+                num = num / nsk;
+                den = den / nsk;
+            }
+            Numerator = num;
+            Denominator = den;
+
+        }
+        public static implicit operator double(Fraction a)
+        {
+            return (double)a.Numerator / a.Denominator;
+        }
+        public static long NSK(long a,long b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return a + b;
+            }
+            else
+                return NSK(b, a % b);
+            
+        }
+        public override string ToString()
+        {
+            return Numerator+"/"+ Denominator;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
